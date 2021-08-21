9f19c2d489bed23768d170fa315ba976c4e09e45
CJ McCaskill <cjmccaskill@gmail.com>
2021-08-21 13:41:36 -0500

    feature-app - add routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,69 @@
 import React, { useState, useEffect } from "react";
-import { Switch, Link, Route } from "react-router-dom"
+import { Switch, Link, Route } from "react-router-dom";
 import "./App.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Agents from "./components/Agents/Agents";
+import Packages from "./components/Packages/Packages";
+import Home from "./pages/Home/Home";
 
 function App() {
+  const url = "https://act-travel-project-api.herokuapp.com/";
+
+  // Call the TripDetails API
+  const [packages, setPackages] = useState([]);
+
+  const getPackages = async () => {
+    const response = await fetch(url + "/tripDetails");
+    const data = await response.json();
+    setPackage(data);
+  };
+
+  useEffect(() => {
+    getPackages;
+  }, []);
+
+  // Call to Agents API
+  const [agents, setAgents] = useState([]);
+
+  const getAgents = async () => {
+    const response = await fetch(url + "/agents");
+    const data = await response.json();
+    setAgent(data);
+  };
+
+  useEffect(() => {
+    getAgents;
+  }, []);
+
   return (
     <div className="App">
-      <h1> 🏝☀️☀️ Welcome to Team ACT's - Travel Project ☀️☀️🏝</h1>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Home packages={packages} agents={agents} />
+          </Route>
+          <Route
+            exact
+            path="/tripDetails/:id"
+            render={(rp) => <Packages {...rp} selectPackage={selectPackage} />}
+          />
+          <Route
+            exact
+            path="/agent/:id"
+            render={(rp) => <Agents {...rp} selectAgent={selectAgent} />}
+          />
+          <Route
+            exact
+            path="/user/userProfile"
+            render={(rp) => <UserProfile {...rp} />}
+          />
+          <Route path="/user/Login" render={(rp) => <UserLogin {...rp} />} />
+          <Route path="/user/SignUp" render={(rp) => <UserLogin {...rp} />} />
+        </Switch>
+      </main>
+      <Footer />
     </div>
   );
 }
