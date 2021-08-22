7ad27a31154ad862a247250a5f08e9b95908ed3b
CJ McCaskill <cjmccaskill@gmail.com>
2021-08-21 19:04:01 -0500

    feature-agent - add map function to agents comp

--- src/components/Agents/Agents.jsx
+++ src/components/Agents/Agents.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Agents.scss";
 
-const Agents = () => {
-  return <div>The top Agencies in the world!</div>;
+const Agents = (props) => {
+  console.log("Agents props -", props);
+  const { agents } = props;
+  return (
+    <div className="agent-container">
+      {agents.map((agent) => (
+        <div key={agent._id}>
+          <div>{agent.img}</div>
+          <div>{agent.name}</div>
+          <div>{agent.contactInfo[0]}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Agents;
