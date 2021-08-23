a9ad83bfa06be1c9acd50eaee32ab3608f786881
traceannwhite <traceannwhite@gmail.com>
2021-08-23 13:11:00 -0500

    Feature-header - Updated with functionality and styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { BiUserCircle } from "react-icons/bi";
+import { CgMenuLeft } from "react-icons/cg";
+import { RiArrowLeftFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => {
-  return <div>Travel Header Comp</div>;
+  // set up useState and set the menu state to closed
+  const [menuOpen, setMenuOpen] = useState(false);
+  // update state when menu is open (! overrides )
+  const handleToggle = () => {
+    setMenuOpen((prev) => !prev);
+  };
+  const hideMenu = () => setMenuOpen(false);
+
+  const navigation = [
+    { link: "/", text: "Home" },
+    { link: "/tripdetails", text: "Shop Packages" },
+    { link: "/agent", text: "Explore Agents" },
+    { link: "/user/userprofile", text: "My Account" },
+  ];
+  return (
+    <div className="header">
+      <div className="nav-bar">
+        {/* need an onClick event in the button to set the state */}
+        <button className="menu-button" open={menuOpen} onClick={handleToggle}>
+          {menuOpen ? (
+            <RiArrowLeftFill className="back-arrow" />
+          ) : (
+            <CgMenuLeft className="hamburger" />
+          )}
+        </button>
+        <ul className={`menu-links ${menuOpen ? " show-menu" : ""}`}>
+          {navigation.map((nav) => (
+            <li key={nav.text}>
+              <Link
+                to={nav.link}
+                activeClassName="active-link"
+                onClick={() => hideMenu()}
+                exact
+              >
+                {nav.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <button className="account-info">
+          <Link to="/user/userprofile">
+            <BiUserCircle className="account-icon" />
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
