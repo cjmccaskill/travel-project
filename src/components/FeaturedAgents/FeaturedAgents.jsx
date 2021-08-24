b0f526711a807d0507796239739cccaf36ab3942
CJ McCaskill <cjmccaskill@gmail.com>
2021-08-23 22:34:04 -0500

    feature-FeaturesAgent - add styling to cards

--- src/components/FeaturedAgents/FeaturedAgents.jsx
+++ src/components/FeaturedAgents/FeaturedAgents.jsx
@@ -12,13 +12,15 @@ const FeaturedAgents = (props) => {
           See All
         </Link>
       </div>
-      <div className="agent-container">
+      <div className="card-container">
         {agents.map((agent) => (
-          <div className="agent-card" key={agent._id}>
-            <Link to={`/agent/${agent._id}`}>
-              <img src={agent.img} alt={agent.name} />
-              <div>{agent.name}</div>
-              <div>More Info</div>
+          <div className="agent-container" key={agent._id}>
+            <Link className="agent-card" to={`/agent/${agent._id}`}>
+              <img src={agent.img} alt={agent.name} className="agent-img" />
+              <div className="card-info">
+                <div className="agent-name">{agent.name}</div>
+                <div className="more-info">More Info</div>
+              </div>
             </Link>
           </div>
         ))}
