4bb61240926d1dd9546f29f55c0017dce951ee88
traceannwhite <traceannwhite@gmail.com>
2021-08-24 18:55:57 -0500

    Trip Packages have been styled for mobile

--- src/components/Packages/Packages.jsx
+++ src/components/Packages/Packages.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaCalendarAlt } from "react-icons/fa";
 import "./Packages.scss";
 
 const Packages = (props) => {
   const { packages } = props;
   return (
     <div>
-      <h1>Trip Packages</h1>
+      <h1 className="tripdetails-head">Trip Packages</h1>
       <div className="tripdetails-container">
         {packages.map((pkg) => (
           <Link
             to={`/tripDetails/${pkg._id}`}
-            className="pkg-tripdetails"
+            className="pkg-tripdetails-card"
             key={pkg._id}
           >
             <img src={pkg.img} alt={pkg.name} className="pkg-img-tripdetails" />
             <div className="pkg-info-tripdetails">
-              <h1>{pkg.name}</h1>
-              <div className="pkg-dates-tripdetails">{pkg.dates}</div>
+              <h1>{pkg.packageName}</h1>
+              <div className="pkg-dates-tripdetails">
+                <FaCalendarAlt className="cal-icon" />
+                {pkg.dates}
+              </div>
             </div>
             <div className="price-tripdetails">
-              <h1>$</h1>
-              {pkg.payment}
+              <div className="price-con">
+                <h1>$</h1>
+                <h1>{pkg.payment}</h1>
+              </div>
             </div>
           </Link>
         ))}
