7c432d01f43be7ceb4e2fb2d9e64d1d360a46c36
traceannwhite <traceannwhite@gmail.com>
2021-08-30 19:30:31 -0500

    Links added to Agent page

--- src/components/Agents/Agents.jsx
+++ src/components/Agents/Agents.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FaPaperPlane, FaLinkedin, FaGlobeAmericas } from "react-icons/fa";
 import "./Agents.scss";
 
@@ -11,19 +12,21 @@ const Agents = (props) => {
       <div className="agents-container">
         {agents.map((agent) => (
           <div className="agents-card" key={agent._id}>
-            <img className="agents-img" src={agent.img} alt={agent.name} />
-            <h2 className="agents-name">{agent.name}</h2>
-            <div className="agents-featured-pkg">
-              <strong>Featured Package: </strong>
-              {
-                <img
-                  src={agent.packages[0].img}
-                  alt={agent.packages[0].packageName}
-                />
-              }
-              <span>{agent.packages[0].packageName}</span>
-            </div>
-            <div className="agents-bio">{agent.bio}</div>
+            <Link to={`/agent/${agent._id}`} className="agents-card">
+              <img className="agents-img" src={agent.img} alt={agent.name} />
+              <h2 className="agents-name">{agent.name}</h2>
+              <div className="agents-featured-pkg">
+                <strong>Featured Package: </strong>
+                {
+                  <img
+                    src={agent.packages[0].img}
+                    alt={agent.packages[0].packageName}
+                  />
+                }
+                <span>{agent.packages[0].packageName}</span>
+              </div>
+              <div className="agents-bio">{agent.bio}</div>
+            </Link>
             <div className="agents-links">
               <a href={`mailto:${agent.email}`} target="_blank">
                 <FaPaperPlane className="agent-links" />
